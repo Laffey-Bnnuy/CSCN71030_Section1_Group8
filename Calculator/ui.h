f25c871a7338602c6c1a15e620ae0edc9d474e11
Mezeh9481 <Mezeh9481@conestogac.on.ca>
2025-03-19 14:03:43 -0400

    Added function prototypes in ui.h

--- Calculator/ui.h
+++ Calculator/ui.h
@@ -1,1 +1,24 @@
 #pragma once
+#include <stdbool.h>
+
+// Function to display the calculator menu
+void displayMenu();
+
+// Function to handle user choices in the menu
+void handleUserChoice();
+
+// Function to accept user input
+void acceptUserInput(char* inputBuffer, int bufferSize);
+
+// Function to validate user input
+void validateInput(const char *expression);
+
+// Function to send expression to computation module
+double sendToComputationModule(const char* expression);
+
+//Function do display the compued result
+void displayResult(double result);
+
+// Function to hanlde and display error messages
+void handleError(const char* errorMessage);
+
